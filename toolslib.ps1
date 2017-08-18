1c58759ce941910ae91b4396226e2f1f4bc4d356
Balakrishnan (Balki) Ranganathan <balki@live.com>
2017-08-18 10:38:10 +0200

    Update commit to include pushing

--- toolslib.ps1
+++ toolslib.ps1
@@ -1,14 +1,15 @@
 param(
-    [string] $CommitMessage,
+    [string] $Commit,
     [switch] $Update
 )
 
 Push-Location src/toolslib
 
-if (![string]::IsNullOrEmpty($CommitMessage)) {
-    git commit --all -m $CommitMessage
+if (![string]::IsNullOrEmpty($Commit)) {
+    git commit --all -m $Commit
 }
 elseif ($Update -eq $true) {
     Pop-Location
     git submodule update --recursive --remote
+    git push origin HEAD:master
 }
